4d4eb2c514ab6974559d010d3028356b4dd57972
Pako Herrera <pakman_198@hotmail.com>
2019-10-09 11:48:10 -0700

    Added navigation creted a Page component which works as a wrapper that injects the theme and and other styled components

--- frontend/components/styles/NavStyles.js
+++ frontend/components/styles/NavStyles.js
@@ -18,14 +18,14 @@ const NavStyles = styled.ul`
     background: none;
     border: 0;
     cursor: pointer;
-    @media (max-width: 700px) {
+    @media (max-width: 767px) {
       font-size: 10px;
-      padding: 0 10px;
+      /* padding: 0 10px; */
     }
     &:before {
       content: '';
       width: 2px;
-      background: ${props => props.theme.lightgrey};
+      background: ${props => props.theme.lightGrey};
       height: 100%;
       left: 0;
       position: absolute;
@@ -35,7 +35,7 @@ const NavStyles = styled.ul`
     }
     &:after {
       height: 2px;
-      background: red;
+      background: ${props => props.theme.primary};
       content: '';
       width: 0;
       position: absolute;
@@ -43,7 +43,7 @@ const NavStyles = styled.ul`
       transition: width 0.4s;
       transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
       left: 50%;
-      margin-top: 2rem;
+      margin-top: 1.3rem;
     }
     &:hover,
     &:focus {
@@ -51,13 +51,13 @@ const NavStyles = styled.ul`
       &:after {
         width: calc(100% - 60px);
       }
-    @media (max-width: 700px) {
-        width: calc(100% - 10px);
-    }
+      @media (max-width: 767px) {
+          /* width: calc(100% - 10px); */
+      }
     }
   }
-  @media (max-width: 1300px) {
-    border-top: 1px solid ${props => props.theme.lightgrey};
+  @media (max-width: 767px) {
+    border-top: 1px solid ${props => props.theme.lightGrey};
     width: 100%;
     justify-content: center;
     font-size: 1.5rem;
