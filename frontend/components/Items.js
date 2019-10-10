d69f111fa296dbe151b0a5286b3279d176ed32bd
Pako Herrera <pakman_198@hotmail.com>
2019-10-10 15:35:25 -0700

    Added an item list component and an item deail compoenent to display the data. Connected React with apollo to make graphql queries and fetch data from the database. Updated some style files since they had a wrong property name hence some styling wasnt applied

---
+++ frontend/components/Items.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+
+import Item from './Item';
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+class Items extends Component {
+
+  renderItems(items) {
+    const els =  items.map(item => <Item key={item.id} item={item} /> );
+
+    return (
+      <ItemList>
+        { els }
+      </ItemList>
+    );
+  }
+
+  render() {
+    return (
+      <Center>
+        <h1>ITEMS!</h1>
+        <Query query={ALL_ITEMS_QUERY}>
+          { 
+            ({ data, error, loading }) => {
+              if(loading) return <p>Loading...</p>;
+              if(error) return <p>Error: {error.message}</p>;
+
+              return this.renderItems(data.items)
+            }
+          }
+        </Query>
+      </Center>
+    )
+  }
+}
+
+export default Items;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,35 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+
+import withData from '../lib/withData';
 
 import Page from '../components/Page';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx}) {
+    let pageProps =  {}
+    
+    if(Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+
+    return pageProps;
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
-      <Container>
+      <ApolloProvider client={apollo}>
         <Page>
-          <Component />
+          <Component {...pageProps} />
         </Page>
-      </Container>
+      </ApolloProvider>
     )
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
